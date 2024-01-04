3ffd0bf146764bfb20f6e6efc58ea4811505e744
Kevin Aquino <ahorasoysegundo@gmail.com>
2024-01-04 15:24:15 -0600

    custom button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -63,6 +63,7 @@ class _ButtonsView extends StatelessWidget {
                 label: const Text('TextIconButton')),
 
             //TODO:
+            const CustomButton(),
 
             IconButton(
                 onPressed: () {},
@@ -75,6 +76,31 @@ class _ButtonsView extends StatelessWidget {
                   iconColor: const MaterialStatePropertyAll(Colors.white)),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'CustomButton UwU',
+                style: TextStyle(color: Colors.white),
+              )),
         ),
       ),
     );
